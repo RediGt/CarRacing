a0a79df6711b31c1bb4b34f0182c1fd2a995aaf4
Redi <red.i@inbox.lv>
2020-07-17 11:25:51 +0300

    Add project files.

---
+++ CarRacing/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CarRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBoxLine1 = new System.Windows.Forms.PictureBox();
+            this.picBoxLine2 = new System.Windows.Forms.PictureBox();
+            this.picBoxLine3 = new System.Windows.Forms.PictureBox();
+            this.picBoxLine4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.picBoxLine5 = new System.Windows.Forms.PictureBox();
+            this.picBoxLine6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxLine1
+            // 
+            this.picBoxLine1.BackColor = System.Drawing.Color.White;
+            this.picBoxLine1.Location = new System.Drawing.Point(138, 1);
+            this.picBoxLine1.Name = "picBoxLine1";
+            this.picBoxLine1.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine1.TabIndex = 0;
+            this.picBoxLine1.TabStop = false;
+            // 
+            // picBoxLine2
+            // 
+            this.picBoxLine2.BackColor = System.Drawing.Color.White;
+            this.picBoxLine2.Location = new System.Drawing.Point(138, 77);
+            this.picBoxLine2.Name = "picBoxLine2";
+            this.picBoxLine2.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine2.TabIndex = 1;
+            this.picBoxLine2.TabStop = false;
+            // 
+            // picBoxLine3
+            // 
+            this.picBoxLine3.BackColor = System.Drawing.Color.White;
+            this.picBoxLine3.Location = new System.Drawing.Point(138, 152);
+            this.picBoxLine3.Name = "picBoxLine3";
+            this.picBoxLine3.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine3.TabIndex = 2;
+            this.picBoxLine3.TabStop = false;
+            // 
+            // picBoxLine4
+            // 
+            this.picBoxLine4.BackColor = System.Drawing.Color.White;
+            this.picBoxLine4.Location = new System.Drawing.Point(138, 231);
+            this.picBoxLine4.Name = "picBoxLine4";
+            this.picBoxLine4.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine4.TabIndex = 3;
+            this.picBoxLine4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(2, -3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(5, 422);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(276, -3);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(5, 417);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // picBoxLine5
+            // 
+            this.picBoxLine5.BackColor = System.Drawing.Color.White;
+            this.picBoxLine5.Location = new System.Drawing.Point(138, 308);
+            this.picBoxLine5.Name = "picBoxLine5";
+            this.picBoxLine5.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine5.TabIndex = 6;
+            this.picBoxLine5.TabStop = false;
+            // 
+            // picBoxLine6
+            // 
+            this.picBoxLine6.BackColor = System.Drawing.Color.White;
+            this.picBoxLine6.Location = new System.Drawing.Point(138, 387);
+            this.picBoxLine6.Name = "picBoxLine6";
+            this.picBoxLine6.Size = new System.Drawing.Size(5, 60);
+            this.picBoxLine6.TabIndex = 7;
+            this.picBoxLine6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.picBoxLine6);
+            this.Controls.Add(this.picBoxLine5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.picBoxLine4);
+            this.Controls.Add(this.picBoxLine3);
+            this.Controls.Add(this.picBoxLine2);
+            this.Controls.Add(this.picBoxLine1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLine6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxLine1;
+        private System.Windows.Forms.PictureBox picBoxLine2;
+        private System.Windows.Forms.PictureBox picBoxLine3;
+        private System.Windows.Forms.PictureBox picBoxLine4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox picBoxLine5;
+        private System.Windows.Forms.PictureBox picBoxLine6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CarRacing/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRacing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveLine(5);
+        }
+
+        void moveLine(int speed)
+        {
+            if (picBoxLine1.Top >= 450)
+                picBoxLine1.Top = 0;
+            else
+                picBoxLine1.Top += speed;
+
+            if (picBoxLine2.Top >= 450)
+                picBoxLine2.Top = 0;
+            else
+                picBoxLine2.Top += speed;
+
+            if (picBoxLine3.Top >= 450)
+                picBoxLine3.Top = 0;
+            else
+                picBoxLine3.Top += speed;
+
+            if (picBoxLine4.Top >= 450)
+                picBoxLine4.Top = 0;
+            else
+                picBoxLine4.Top += speed;
+
+            if (picBoxLine5.Top >= 450)
+                picBoxLine5.Top = 0;
+            else
+                picBoxLine5.Top += speed;
+
+            if (picBoxLine6.Top >= 450)
+                picBoxLine6.Top = 0;
+            else
+                picBoxLine6.Top += speed;
+        }
+    }
+}
